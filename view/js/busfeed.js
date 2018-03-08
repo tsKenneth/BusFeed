47debd3f3229bc8d69278a036480758bd0938d4a
Kenneth Teo <tskennethteo@gmail.com>
2018-03-09 00:28:12 +0800

    Initial Commit
    Initial Commit

---
+++ view/js/busfeed.js
@@ -0,0 +1,6 @@
+function hover(element) {
+    element.setAttribute('src', 'http://dummyimage.com/100x100/eb00eb/fff');
+}
+function unhover(element) {
+    element.setAttribute('src', 'http://dummyimage.com/100x100/000/fff');
+}
