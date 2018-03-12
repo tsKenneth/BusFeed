44c71778f278816604e44e082229ce0a6aa1b26e
Kenneth Teo <tskennethteo@gmail.com>
2018-03-13 01:18:40 +0800

    Models and edited mappers

---
+++ model/Busservice_Model.php
@@ -0,0 +1,79 @@
+<?php
+
+class Busservice {
+    // Properties
+    private string $serviceNo;
+    private string $operator;
+    private int $direction;
+    private string $category;
+    private int $originCode;
+    private int $destinationCode;
+    private string $AM_Peak_Freq;
+    private string $AM_Offpeak_Freq;
+    private string $PM_Peak_Freq;
+    private string $PM_Offpeak_Freq;
+    private string $loopDesc;
+    
+    // Methods
+
+        // Accessors and Mutators
+        
+        // Returns all information in array format
+        public function getInformationArray() {
+            
+        }
+        
+        public function getSeviceNo() {
+            
+        }
+        
+        public function getOperator() {
+            
+        }
+        
+        public function getDirection() {
+            
+        }
+        
+        public function getCategory() {
+            
+        }
+        
+        public function getOriginCode() {
+            
+        }
+        
+        public function getDestinationCode() {
+            
+        }
+        
+        public function getAMDestinationCode() {
+            
+        }
+        
+        public function getAM_Offpeak_Freq() {
+            
+        }
+        
+        public function getPM_Peak_Freq() {
+            
+        }
+        
+        public function getPM_Offpeak_Freq() {
+            
+        }
+        
+        public getLoopDesc() {
+            
+        }
+        
+
+        
+        
+        
+        
+        
+        
+}
+
+?>
---
+++ model/Busstop_Model.php
@@ -0,0 +1,46 @@
+<?php
+
+class Busstop {
+    // Properties
+    private int $busStopCode;
+    private string $roadName;
+    private string $description;
+    private float $latitude;
+    private float $longitude;
+    
+    // Methods
+
+        // Accessors and Mutators
+        
+        // Returns all information in array format
+        public function getInformationArray() {
+            
+        }
+        
+        // Returns Coordinates in array format
+        public function getCoordinatesArray() {
+            
+        }
+        
+        public function getBusstopCode() {
+            
+        }
+        
+        public function getRoadName() {
+            
+        }
+        
+        public function getDescription() {
+            
+        }
+        
+        public function getLatitude() {
+            
+        }
+        
+        public function getLongitude() {
+            
+        }
+}
+
+?>
