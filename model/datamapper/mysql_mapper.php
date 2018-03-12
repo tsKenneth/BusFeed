e5b66275a25cdd09cc3b542ab4224f5ba2614183
Kenneth Teo <tskennethteo@gmail.com>
2018-03-12 22:54:17 +0800

    Added Datamappers

---
+++ model/datamapper/mysql_mapper.php
@@ -0,0 +1,16 @@
+<?php
+
+include 'database_mapper.php';
+
+class MySQL_Mapper implements iDatabase_Mapper
+{
+    public function addBusstop(int $busStopCode, array $busstopInformationArray );
+    public function getBusstop(int $busStopCode);
+    public function removeBusstop(int $busStopCode);
+    
+    public function addBusservice(string $serviceNo, array $busserviceInformationArray );
+    public function getBusservice(string $serviceNo);
+    public function removeBusservice(string $serviceNo);
+}
+
+?>
