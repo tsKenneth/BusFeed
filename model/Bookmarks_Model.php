ef9f1dcba9b7e5d79ed0543e6dfa297cb04f8a95
Kenneth Teo <tskennethteo@gmail.com>
2018-03-13 18:08:05 +0800

    Finished Application Skeleton

---
+++ model/Bookmarks_Model.php
@@ -0,0 +1,28 @@
+<?php
+
+class Bookmarks {
+    // Properties
+    private array $bookmarkedBusstop;
+    private array $bookmarkedBusservice;
+    private string $userID;
+    
+    // Methods
+
+        // Accessors and Mutators
+        // Returns all bookmarked busservice
+        public function getBusserviceArray() {
+            
+        }
+        
+        // Returns all bookmarked busstops
+        public function getBusstopArray() {
+            
+        }
+        
+        // Returns the user id that owns the bookmarks
+        public function getUserID() {
+            
+        }
+}
+
+?>
