defd62efc7bf09ed5f7e043df57452d2f785d92f
Jonathan Kevin Chandra <jk_chandra@hotmail.com>
2018-04-04 00:18:04 +0800

    Updated SQLite Mapper and Models
    Changed some syntax in models and included API for LTADatamall, database mapper, sqlite mapper

--- model/Busroute_Model.php
+++ model/Busroute_Model.php
@@ -1,72 +1,109 @@
 <?php
 
+
+include('../view/php/apiHandlerLTA.php');
+include('datamapper/sqlite_mapper.php');
+
+
 class Busroute {
     // Properties
-    private string $serviceNo;
-    private int $direction;
-    private int $stopSequence;
-    private int $busStopCode;
-    private float $distance;
-    private int $WD_FirstBus;
-    private int $WD_LastBus;
-    private int $SAT_FirstBus;
-    private int $SAT_LastBus;
-    private int $SUN_FirstBus;
-    private int $SUN_LastBus;
-    
+    private $serviceNo;
+    private $operator;
+    private $direction;
+    private $stopSequence;
+    private $busStopCode;
+    private $distance;
+    private $WD_FirstBus;
+    private $WD_LastBus;
+    private $SAT_FirstBus;
+    private $SAT_LastBus;
+    private $SUN_FirstBus;
+    private $SUN_LastBus;
+
     // Methods
 
-        // Accessors and Mutators
-        
-        // Returns all information in array format
-        public function getInformationArray() {
-            
-        }
-        
-        public function getServiceNo() {
-            
-        }
-        
-        public function getDirection() {
-            
-        }
-        
-        public function getStopSequence() {
-            
-        }
-        
-        public function getBusstopCode() {
-            
-        }
-        
-        public function getDistance() {
-            
-        }
-        
-        public function getWD_FirstBus() {
-            
-        }
-        
-        public function getWD_LastBus() {
-            
-        }
-        
-        public function getSAT_FirstBus() {
-            
-        }
-        
-        public function getSAT_LastBus() {
-            
-        }
-        
-        public function getSUN_FirstBus() {
-            
-        }
-        
-        public function getSUN_LastBus() {
-            
-        }
-        
+    // Accessors and Mutators
+
+    // Returns all information in array format
+    public function getInformationArray() {
+
+    }
+
+    public function getServiceNo() {
+
+    }
+
+    public function getOperator(){
+
+    }
+
+    public function getDirection() {
+
+    }
+
+    public function getStopSequence() {
+
+    }
+
+    public function getBusstopCode() {
+
+    }
+
+    public function getDistance() {
+
+    }
+
+    public function getWD_FirstBus() {
+
+    }
+
+    public function getWD_LastBus() {
+
+    }
+
+    public function getSAT_FirstBus() {
+
+    }
+
+    public function getSAT_LastBus() {
+
+    }
+
+    public function getSUN_FirstBus() {
+
+    }
+
+    public function getSUN_LastBus() {
+
+    }
+
+}
+
+function storeBusRoutes(){
+  $mapper = new SQLite_Mapper();
+  $mapper->createBusRouteTable();
+  $busRouteChunks = APIBusRoutes();
+  $tempArray = array();
+  $iter = 0;
+  foreach ($busRouteChunks as $busRouteChunk){
+    $BusRoutes = (json_decode($busRouteChunk))->value;
+    foreach ($BusRoutes as $BusRoute) {
+      $tempArray[$iter][0] = $BusRoute->ServiceNo;
+      $tempArray[$iter][1] = $BusRoute->Operator;
+      $tempArray[$iter][2] = $BusRoute->Direction;
+      $tempArray[$iter][3] = $BusRoute->StopSequence;
+      $tempArray[$iter][4] = $BusRoute->BusStopCode;
+      $tempArray[$iter][5] = $BusRoute->Distance;
+      $tempArray[$iter][6] = $BusRoute->WD_FirstBus;
+      $tempArray[$iter][7] = $BusRoute->WD_LastBus;
+      $tempArray[$iter][8] = $BusRoute->SAT_FirstBus;
+      $tempArray[$iter][9] = $BusRoute->SAT_LastBus;
+      $tempArray[$iter][10] = $BusRoute->SUN_FirstBus;
+      $tempArray[$iter][11] = $BusRoute->SUN_LastBus;
+      $iter = $iter + 1;
+    }
+  }
+  $mapper->addMultBusRoute($tempArray);
 }
 
 ?>
--- model/Busstop_Model.php
+++ model/Busstop_Model.php
@@ -1,46 +1,72 @@
 <?php
 
+include('../view/php/apiHandlerLTA.php');
+include('datamapper/sqlite_mapper.php');
+
 class Busstop {
     // Properties
-    private int $busStopCode;
-    private string $roadName;
-    private string $description;
-    private float $latitude;
-    private float $longitude;
-    
+    private $busStopCode;
+    private $roadName;
+    private $description;
+    private $latitude;
+    private $longitude;
+
     // Methods
 
-        // Accessors and Mutators
-        
-        // Returns all information in array format
-        public function getInformationArray() {
-            
-        }
-        
-        // Returns Coordinates in array format
-        public function getCoordinatesArray() {
-            
-        }
-        
-        public function getBusstopCode() {
-            
-        }
-        
-        public function getRoadName() {
-            
-        }
-        
-        public function getDescription() {
-            
-        }
-        
-        public function getLatitude() {
-            
-        }
-        
-        public function getLongitude() {
-            
-        }
+
+    // Accessors and Mutators
+
+    // Returns all information in array format
+    public function getInformationArray() {
+
+    }
+
+    // Returns Coordinates in array format
+    public function getCoordinatesArray() {
+
+    }
+
+    public function getBusstopCode() {
+
+    }
+
+    public function getRoadName() {
+
+    }
+
+    public function getDescription() {
+
+    }
+
+    public function getLatitude() {
+
+    }
+
+    public function getLongitude() {
+
+    }
+}
+
+//Creates a Busstop table and stores all the Busstops from LTA DataMall to database
+function storeBusstops(){
+  $mapper = new SQLite_Mapper();
+
+  $mapper->createBusStopTable();
+  $busStopChunks = APIBusStops();
+  $tempArray = array();
+  $iter = 0;
+  foreach ($busStopChunks as $busStopChunk){
+    $Busstops = (json_decode($busStopChunk))->value;
+    foreach ($Busstops as $Busstop) {
+      $tempArray[$iter][0] = $Busstop->BusStopCode;
+      $tempArray[$iter][1] = $Busstop->RoadName;
+      $tempArray[$iter][2] = $Busstop->Longitude;
+      $tempArray[$iter][3] = $Busstop->Latitude;
+      $tempArray[$iter][4] = $Busstop->Description;
+      $iter = $iter + 1;
+    }
+  }
+  $mapper->addMultBusstop($tempArray);
 }
 
 ?>
