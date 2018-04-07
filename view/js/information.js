3a4a65fb30a44f0574b65e17624afe4879b09e66
Keith_Ang <kang024@e.ntu.edu.sg>
2018-04-07 16:54:39 +0800

    css

--- view/js/information.js
+++ view/js/information.js
@@ -1,88 +1,191 @@
-var busstopArray;
-var busserviceArray;
-
 function initialiseTable() {
-    retrieveBusstopList();
-    retrieveBusserviceList()
+    var resultsArray = retrieveBusstopList();
+    var resultsArray2 = retrieveBusserviceList();
+    updateBusstopResultsTable(resultsArray);
+    updateBusserviceResultsTable(resultsArray2);
     document.querySelector('#filterBox').addEventListener('keyup',
         filterBusstopTable, false);
     document.querySelector('#filterBox').addEventListener('keyup',
         filterBusserviceTable, false);
 }
 
-$('#informationModal').on('show.bs.modal', function(modal) {
-    var modalType = modal.relatedTarget.dataset.type;
-
-    if (modalType == "busstop") {
-        var busstopCode = modal.relatedTarget.dataset.busstopcode;
-        for (var i=0; i < busstopArray.length; i++) {
-            if (busstopArray[i].busStopCode == busstopCode) {
-                document.getElementById("modalText").innerHTML = `
-                <p>
-                    <b>Busstop Code:</b> ${busstopArray[i].busStopCode} <br />
-                    <b>Road Name:</b> ${busstopArray[i].roadName} <br />
-                    <b>Description:</b> ${busstopArray[i].description} <br />
-                    <b>Latitude:</b> ${busstopArray[i].latitude} <br />
-                    <b>Longitude:</b> ${busstopArray[i].longitude} <br />
-                </p>
-                <hr>
-                <b>Busses</b>
-                <button type="button" class="btn btn-success">Add to bookmarks</button>
-                `;
-            }
-        }
-    } else if(modalType == "busservice") {
-        var serviceno = modal.relatedTarget.dataset.serviceno;
-        var direction = modal.relatedTarget.dataset.direction;
-        for (var i=0; i < busserviceArray.length; i++) {
-            if (busserviceArray[i].serviceNo == serviceno) {
-                document.getElementById("modalText").innerHTML = `
-                <p>
-                    <b>Service Number:</b> ${busserviceArray[i].serviceNo} <br />
-                    <b>Operator:</b> ${busserviceArray[i].operator} <br />
-                    <b>Direction:</b> ${busserviceArray[i].direction} <br />
-                    <b>Category:</b> ${busserviceArray[i].category} <br />
-                    <b>Origin Code:</b> ${busserviceArray[i].originCode} <br />
-                    <b>Destination Code:</b> ${busserviceArray[i].destinationCode} <br />
-                    <b>AM Peak Frequency:</b> ${busserviceArray[i].AM_Peak_Freq} <br />
-                    <b>AM Offpeak Frequency:</b> ${busserviceArray[i].AM_Offpeak_Freq} <br />
-                    <b>PM Peak Frequency:</b> ${busserviceArray[i].PM_Peak_Freq} <br />
-                    <b>PM Offpeak Frequency:</b> ${busserviceArray[i].PM_Offpeak_Freq} <br />
-                    <b>Loop at:</b> ${busserviceArray[i].loopDesc} <br />
-                </p>
-                <button type="button" class="btn btn-success">Add to bookmarks</button>
-
-                `;
-            }
-        }
-    }
-});
-
-function updateBusstopBusArrivalTiming() {
-    $.ajax({
-        url: '../model/requestHandler.php',
-        data: "function=getAllBusstopJSON",
-        contentType: "application/json; charset=utf-8",
-        success: updateBusstopResultsTable
-    });
-}
-
 function retrieveBusstopList() {
-    $.ajax({
-        url: '../model/requestHandler.php',
-        data: "function=getAllBusstopJSON",
-        contentType: "application/json; charset=utf-8",
-        success: updateBusstopResultsTable
-    });
+    var results = [
+        {
+            busstopcode:"481",
+            roadname:"Woodlands Rd",
+            latitude:"1.37835675026421",
+            longitude:"103.743085058036",
+            description:"BT PANJANG TEMP BUS PK"
+        },
+        {
+            busstopcode:"1012",
+            roadname:"VictoriaSt",
+            latitude:"1.29684825487647",
+            longitude:"103.85253591654",
+            description:"Hotel Grand Pacific"
+        },
+        {
+            busstopcode:"1029",
+            roadname:"Nth Bridge Rd",
+            latitude:"1.297504044",
+            longitude:"103.854414224643",
+            description:"Cosmic Insurance Bldg"
+        },
+        {
+            busstopcode:"1119",
+            roadname:"Victoria St",
+            latitude:"1.2996041093804",
+            longitude:"103.855129340796",
+            description:"Bugis Junction"
+        },
+        {
+            busstopcode:"1121",
+            roadname:"Victoria St",
+            latitude:"1.30393809691048",
+            longitude:"103.858679992432",
+            description:"Stamford Pr Sch"
+        },
+        {
+            busstopcode:"1119",
+            roadname:"Victoria St",
+            latitude:"1.2996041093804",
+            longitude:"103.855129340796",
+            description:"Bugis Junction"
+        },
+        {
+            busstopcode:"1119",
+            roadname:"Victoria St",
+            latitude:"1.2996041093804",
+            longitude:"103.855129340796",
+            description:"Bugis Junction"
+        },
+        {
+            busstopcode:"1119",
+            roadname:"Victoria St",
+            latitude:"1.2996041093804",
+            longitude:"103.855129340796",
+            description:"Bugis Junction"
+        },
+        {
+            busstopcode:"1119",
+            roadname:"Victoria St",
+            latitude:"1.2996041093804",
+            longitude:"103.855129340796",
+            description:"Bugis Junction"
+        },
+        {
+            busstopcode:"1119",
+            roadname:"Victoria St",
+            latitude:"1.2996041093804",
+            longitude:"103.855129340796",
+            description:"Bugis Junction"
+        }
+    ];
+    return results;
 }
 
 function retrieveBusserviceList() {
-    $.ajax({
-        url: '../model/requestHandler.php',
-        data: "function=getAllBusserviceJSON",
-        contentType: "application/json; charset=utf-8",
-        success: updateBusserviceResultsTable
-    });
+    var results = [
+        {
+            serviceno:"118",
+            direction:"1",
+            origincode:"65009",
+            destinationcode:"97009",
+            loopdescription:"",
+            operator:"GAS"
+        },
+        {
+            serviceno:"118",
+            direction:"2",
+            origincode:"97009",
+            destinationcode:"65009",
+            loopdescription:"",
+            operator:"GAS"
+        },
+        {
+            serviceno:"119",
+            direction:"1",
+            origincode:"65009",
+            destinationcode:"65009",
+            loopdescription:"Hougang St 21",
+            operator:"GAS"
+        },
+        {
+            serviceno:"118",
+            direction:"1",
+            origincode:"65009",
+            destinationcode:"97009",
+            loopdescription:"",
+            operator:"GAS"
+        },
+        {
+            serviceno:"118",
+            direction:"2",
+            origincode:"97009",
+            destinationcode:"65009",
+            loopdescription:"",
+            operator:"GAS"
+        },
+        {
+            serviceno:"119",
+            direction:"1",
+            origincode:"65009",
+            destinationcode:"65009",
+            loopdescription:"Hougang St 21",
+            operator:"GAS"
+        },
+        {
+            serviceno:"118",
+            direction:"1",
+            origincode:"65009",
+            destinationcode:"97009",
+            loopdescription:"",
+            operator:"GAS"
+        },
+        {
+            serviceno:"118",
+            direction:"2",
+            origincode:"97009",
+            destinationcode:"65009",
+            loopdescription:"",
+            operator:"GAS"
+        },
+        {
+            serviceno:"119",
+            direction:"1",
+            origincode:"65009",
+            destinationcode:"65009",
+            loopdescription:"Hougang St 21",
+            operator:"GAS"
+        },
+        {
+            serviceno:"118",
+            direction:"1",
+            origincode:"65009",
+            destinationcode:"97009",
+            loopdescription:"",
+            operator:"GAS"
+        },
+        {
+            serviceno:"118",
+            direction:"2",
+            origincode:"97009",
+            destinationcode:"65009",
+            loopdescription:"",
+            operator:"GAS"
+        },
+        {
+            serviceno:"119",
+            direction:"1",
+            origincode:"65009",
+            destinationcode:"65009",
+            loopdescription:"Hougang St 21",
+            operator:"GAS"
+        }
+
+    ];
+    return results;
 }
 
 function filterBusstopTable(event) {
@@ -109,8 +212,8 @@ function filterBusserviceTable(event) {
     for (var i = 0; i < rows.length; i++) {
         var busservicenoCol = rows[i].cells[0].textContent.toUpperCase();
         var originCol = rows[i].cells[1].textContent.toUpperCase();
-        var destinationCol = rows[i].cells[2].textContent.toUpperCase();
-        var loopCol = rows[i].cells[3].textContent.toUpperCase();
+        var destinationCol = rows[i].cells[3].textContent.toUpperCase();
+        var loopCol = rows[i].cells[4].textContent.toUpperCase();
         if (busservicenoCol.indexOf(filter) > -1
             || originCol.indexOf(filter) > -1 || destinationCol.indexOf(filter) > -1
             || loopCol.indexOf(filter) > -1) {
@@ -121,8 +224,7 @@ function filterBusserviceTable(event) {
     }
 }
 
-function updateBusserviceResultsTable(results, status, xhr) {
-    var resultsArray = JSON.parse(results);
+function updateBusserviceResultsTable(resultsArray) {
     var tableString = `
     <div class=\"table-responsive\">
         <table class=\"table\" id=\"busserviceTable\">
@@ -130,6 +232,7 @@ function updateBusserviceResultsTable(results, status, xhr) {
                 <tr>
                     <th scope=\"col\">Bus Service No.</th>
                     <th scope=\"col\">Origin</th>
+                    <th scope=\"col\"></th>
                     <th scope=\"col\">Destination</th>
                     <th scope=\"col\">Loop at</th>
                 </tr>
@@ -139,13 +242,12 @@ function updateBusserviceResultsTable(results, status, xhr) {
     resultsArray.forEach(
         function (arrayItem) {
             tableString += `
-                <tr data-toggle=\"modal\" data-target=\"#informationModal\"
-                    data-type=\"busservice\" data-serviceno=\"${arrayItem.serviceNo}\"
-                    data-direction=\"${arrayItem.direction}\">
-                    <th scope=\"row\">${arrayItem.serviceNo}</th>
-                    <td>${arrayItem.originCode}</td>
-                    <td>${arrayItem.destinationCode}</td>
-                    <td>${arrayItem.loopDesc}</td>
+                <tr>
+                    <th scope=\"row\">${arrayItem.serviceno}</th>
+                    <td>${arrayItem.origincode}</td>
+                    <td>${arrayItem.direction}</td>
+                    <td>${arrayItem.destinationcode}</td>
+                    <td>${arrayItem.loopdescription}</td>
                 </tr>
             `
     });
@@ -155,13 +257,13 @@ function updateBusserviceResultsTable(results, status, xhr) {
         </table>
     </div>
     `
-    busserviceArray = resultsArray;
+
+    console.log(tableString);
     document.getElementById("busservice").innerHTML = tableString;
 }
 
 
-function updateBusstopResultsTable(results, status, xhr) {
-    var resultsArray = JSON.parse(results);
+function updateBusstopResultsTable(resultsArray) {
     var tableString = `
     <div class=\"table-responsive\">
         <table class=\"table\" id=\"busstopTable\">
@@ -177,10 +279,9 @@ function updateBusstopResultsTable(results, status, xhr) {
     resultsArray.forEach(
         function (arrayItem) {
             tableString += `
-                <tr data-toggle=\"modal\" data-target=\"#informationModal\"
-                    data-type=\"busstop\" data-busstopcode=\"${arrayItem.busStopCode}\">
-                    <th scope=\"row\">${arrayItem.busStopCode}</th>
-                    <td>${arrayItem.roadName}</td>
+                <tr>
+                    <th scope=\"row\">${arrayItem.busstopcode}</th>
+                    <td>${arrayItem.roadname}</td>
                     <td>${arrayItem.description}</td>
                 </tr>
             `
@@ -192,6 +293,6 @@ function updateBusstopResultsTable(results, status, xhr) {
     </div>
     `
 
-    busstopArray = resultsArray;
+    console.log(tableString);
     document.getElementById("busstop").innerHTML = tableString;
 }
